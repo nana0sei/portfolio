50f302fa3da3a4515a5882fa224e3ff92c005995
nana0sei <frimpongnanao@gmail.com>
2023-10-31 20:52:14 +0000

    logo change

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,11 @@
 import { HStack, Image } from "@chakra-ui/react";
 import ColorModeSwitch from "./ColorModeSwitch";
-import nBlack from "../assets/nBlack.png";
+import logo from "../assets/logo.png";
 
 const Navbar = () => {
   return (
     <HStack justifyContent="space-between" padding="10px">
-      <Image src={nBlack} boxSize="60px" objectFit="cover" />
+      <Image src={logo} boxSize="60px" objectFit="cover" />
       <ColorModeSwitch />
     </HStack>
   );

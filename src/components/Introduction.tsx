c2dcd44f4ce188c7dd61587b89472061f95b41b4
nana0sei <frimpongnanao@gmail.com>
2023-10-31 21:26:48 +0000

    introductory text

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -3,8 +3,8 @@ import { Heading, VStack, Text } from "@chakra-ui/react";
 const Introduction = () => {
   return (
     <>
-      <VStack alignContent="center" spacing={5}>
-        <Heading size="3xl" textAlign="center">
+      <VStack spacing={5} textAlign="center">
+        <Heading size="3xl">
           hi, my name is nana and i'm a front end developer and digital artist.
         </Heading>
         <Text>

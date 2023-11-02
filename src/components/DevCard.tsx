61e90b3fd6cb8c4eec16abf3f2175f362daf48c7
nana0sei <frimpongnanao@gmail.com>
2023-11-02 19:51:57 +0000

    project cards

---
+++ src/components/DevCard.tsx
@@ -0,0 +1,31 @@
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+interface Props {
+  link: string;
+  name: string;
+  description: string;
+  image: string;
+}
+
+const ProjectCard = ({ name, description, link, image }: Props) => {
+  return (
+    <>
+      <Card width="300px">
+        <Image src={image} />
+        <CardBody>
+          <Link
+            to={`https://github.com/nana0sei${link}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Heading fontSize="2xl">{name}</Heading>
+          </Link>
+          <Text as="i">{description}</Text>
+        </CardBody>
+      </Card>
+    </>
+  );
+};
+
+export default ProjectCard;
--- src/pages/DevPage.tsx
+++ src/pages/DevPage.tsx
@@ -1,9 +1,17 @@
 import { Heading } from "@chakra-ui/react";
+import DevCard from "../components/DevCard";
+import gamehub from "../assets/gamehub.png";
 
 const DevPage = () => {
   return (
     <>
-      <Heading>Dev Page</Heading>
+      <Heading paddingBottom={5}>Dev Page</Heading>
+      <DevCard
+        name="Gamehub"
+        description="A lite version of rawg.io, a game discovery website."
+        link="/game-hub"
+        image={gamehub}
+      />
     </>
   );
 };

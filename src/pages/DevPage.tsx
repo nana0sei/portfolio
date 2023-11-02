e8b2dafc1269337805096feec28a982e2dd924c6
nana0sei <frimpongnanao@gmail.com>
2023-11-02 20:47:01 +0000

    dev page

--- src/pages/DevPage.tsx
+++ src/pages/DevPage.tsx
@@ -1,13 +1,9 @@
-import { Heading } from "@chakra-ui/react";
 import DevCard from "../components/DevCard";
 import gamehub from "../assets/gamehub.png";
 
 const DevPage = () => {
   return (
     <>
-      <Heading fontSize="4xl" paddingBottom={5}>
-        Front End Development Projects
-      </Heading>
       <DevCard
         name="Gamehub"
         description="A lite version of rawg.io, a game discovery website."

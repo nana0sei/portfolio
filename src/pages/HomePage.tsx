d5d56419026a4e4717ce3f131758d3794cfc2f57
nana0sei <frimpongnanao@gmail.com>
2023-11-04 00:10:36 +0000

    homepage links

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,17 @@
+import { Image, SimpleGrid } from "@chakra-ui/react";
 import Introduction from "../components/Introduction";
+import dev from "../assets/dev.png";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <>
       <Introduction />
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={5}>
+        <Link to="/dev-projects">
+          <Image src={dev} />
+        </Link>
+      </SimpleGrid>
     </>
   );
 };

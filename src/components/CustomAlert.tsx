d3e93c956d9543274680a5445e065216e8671e81
nana0sei <frimpongnanao@gmail.com>
2023-11-04 21:28:30 +0000

    custom alert

---
+++ src/components/CustomAlert.tsx
@@ -0,0 +1,30 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalBody,
+  Alert,
+} from "@chakra-ui/react";
+import { ReactNode } from "react";
+
+interface Props {
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+}
+const CustomAlert = ({ isOpen, onClose, children }: Props) => {
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <Alert status="info" variant="solid">
+            {children}
+          </Alert>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default CustomAlert;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,22 @@
-import { Image, SimpleGrid } from "@chakra-ui/react";
+import {
+  Image,
+  SimpleGrid,
+  useDisclosure,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import Introduction from "../components/Introduction";
 import dev from "../assets/dev.png";
 import art from "../assets/art.png";
+import locked from "../assets/locked.png";
 import unknown from "../assets/unknown.png";
 import { Link } from "react-router-dom";
 import HoverContainer from "../components/HoverContainer";
+import CustomAlert from "../components/CustomAlert";
 
 const HomePage = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <>
       <Introduction />
@@ -24,9 +34,16 @@ const HomePage = () => {
         </HoverContainer>
 
         <HoverContainer>
-          <Image src={unknown} />
+          <Image src={unknown} onClick={onOpen} />
         </HoverContainer>
       </SimpleGrid>
+
+      <CustomAlert isOpen={isOpen} onClose={onClose}>
+        <VStack spacing={2}>
+          <Image src={locked} />
+          <Text as="b">this section is not yet available.</Text>
+        </VStack>
+      </CustomAlert>
     </>
   );
 };

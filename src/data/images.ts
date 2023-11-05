19e7bb74f396cef8e30027600f8772c5ebe8c78c
nana0sei <frimpongnanao@gmail.com>
2023-11-05 06:25:29 +0000

    image optimization

--- src/data/images.ts
+++ src/data/images.ts
@@ -1,15 +1,15 @@
-import tems from "../assets/art/tems.png";
-import city from "../assets/art/city.png";
-import onfire from "../assets/art/onfire.png";
-import fire from "../assets/art/fire.png";
+import tems from "../assets/art/tems.jpg";
+import city from "../assets/art/city.jpg";
+import onfire from "../assets/art/onfire.jpg";
+import fire from "../assets/art/fire.jpg";
 import sony from "../assets/art/sony.jpg";
-import rita from "../assets/art/rita.png";
-import ray from "../assets/art/ray.png";
+import rita from "../assets/art/rita.jpg";
+import ray from "../assets/art/ray.jpg";
 import krpt from "../assets/art/krpt.jpg";
 import casestody from "../assets/art/casestody.jpg";
-import weeknd from "../assets/art/weeknd.png";
-import recap1 from "../assets/art/recap1.png";
-import recap2 from "../assets/art/recap2.png";
+import weeknd from "../assets/art/weeknd.jpg";
+import recap1 from "../assets/art/recap1.jpg";
+import recap2 from "../assets/art/recap2.jpg";
 
 export default {
   data: [

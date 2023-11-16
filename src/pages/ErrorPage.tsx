9d57ab733094e047fb78fca7c95890cc95de7410
nana0sei <frimpongnanao@gmail.com>
2023-11-16 20:17:26 +0000

    error message cleanup

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,14 +1,20 @@
 import Navbar from "../components/Navbar";
 import error from "../assets/error.png";
 import { VStack, Image, Heading } from "@chakra-ui/react";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
+  const errorType = useRouteError();
   return (
     <>
       <Navbar />
-      <VStack paddingX={5}>
-        <Image src={error} width="1000px" />
-        <Heading>page not found</Heading>
+      <VStack paddingX={5} pt="70px">
+        <Image src={error} width="500px" />
+        <Heading fontSize="lg">
+          {isRouteErrorResponse(errorType)
+            ? "page not found"
+            : "unexpected error occured"}
+        </Heading>
       </VStack>
     </>
   );

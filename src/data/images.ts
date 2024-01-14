c3c5e27a678029c140c36a497d853a3d448ddef7
nana0sei <frimpongnanao@gmail.com>
2024-01-14 21:13:18 +0000

    optimized image sizes

--- src/data/images.ts
+++ src/data/images.ts
@@ -1,15 +1,10 @@
-import tems from "../assets/art/tems.jpg";
 import city from "../assets/art/city.jpg";
 import onfire from "../assets/art/onfire.jpg";
-import fire from "../assets/art/fire.jpg";
 import sony from "../assets/art/sony.jpg";
 import rita from "../assets/art/rita.jpg";
 import ray from "../assets/art/ray.jpg";
 import krpt from "../assets/art/krpt.jpg";
 import casestody from "../assets/art/casestody.jpg";
-import weeknd from "../assets/art/weeknd.jpg";
-import recap1 from "../assets/art/recap1.jpg";
-import recap2 from "../assets/art/recap2.jpg";
 
 export default {
   data: [
@@ -17,13 +12,8 @@ export default {
     { image: ray },
     { image: rita },
     { image: sony },
-    { image: tems },
+    { image: krpt },
     { image: city },
     { image: casestody },
-    { image: weeknd },
-    { image: krpt },
-    { image: recap1 },
-    { image: recap2 },
-    { image: fire },
   ],
 };

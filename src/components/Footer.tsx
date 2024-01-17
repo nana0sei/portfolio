ab1b978ec74ce16f77bc264c567c1a8d69c40a73
nana0sei <frimpongnanao@gmail.com>
2024-01-17 19:39:09 +0000

    rearranged external links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,45 @@
-import { AbsoluteCenter, Box, Divider } from "@chakra-ui/react";
+import { AbsoluteCenter, Box, Center, Divider, HStack } from "@chakra-ui/react";
+import { AiFillGithub } from "react-icons/ai";
+import { FaXTwitter } from "react-icons/fa6";
+import { IoIosMail } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
     <>
-      <Box pos="relative" p="10">
+      <Box pos="relative" px="5" py={5}>
         <Divider />
         <AbsoluteCenter px="4">© {year} Nana Osei</AbsoluteCenter>
       </Box>
+      <Center>
+        <HStack spacing={2}>
+          <Link
+            to="mailto:frimpongnanao@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IoIosMail size="30px" />
+          </Link>
+
+          <Link
+            to="https://github.com/nana0sei/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AiFillGithub size="25px" />
+          </Link>
+
+          <Link
+            to="https://x.com/nana0sei/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaXTwitter size="25px" />
+          </Link>
+        </HStack>
+      </Center>
     </>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,8 @@
-import { Box, HStack, Image, useColorMode } from "@chakra-ui/react";
+import { HStack, Image, useColorMode } from "@chakra-ui/react";
 import ColorModeSwitch from "./ColorModeSwitch";
 import logoW from "../assets/logoW.png";
 import logoB from "../assets/logoB.png";
 import { Link } from "react-router-dom";
-import { AiFillGithub } from "react-icons/ai";
-import { FaXTwitter } from "react-icons/fa6";
 
 const Navbar = () => {
   const { colorMode } = useColorMode();
@@ -26,25 +24,7 @@ const Navbar = () => {
           objectFit="cover"
         />
       </Link>
-      <Box>
-        <HStack>
-          <Link
-            to="https://github.com/nana0sei/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <AiFillGithub size="20px" />
-          </Link>
-          <Link
-            to="https://x.com/nana0sei/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaXTwitter size="20px" />
-          </Link>
-          <ColorModeSwitch />
-        </HStack>
-      </Box>
+      <ColorModeSwitch />
     </HStack>
   );
 };

1a0250352947e720430eef408b563d85a9649672
nana0sei <frimpongnanao@gmail.com>
2024-04-06 22:19:10 +0000

    removed unknown page button

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,26 +1,16 @@
-import {
-  Image,
-  SimpleGrid,
-  useDisclosure,
-  Text,
-  VStack,
-} from "@chakra-ui/react";
+import { Image, SimpleGrid } from "@chakra-ui/react";
 import Introduction from "../components/Introduction";
 import dev from "../assets/dev.png";
 import art from "../assets/art.png";
-import locked from "../assets/locked.png";
-import unknown from "../assets/unknown.png";
+
 import { Link } from "react-router-dom";
 import HoverContainer from "../components/HoverContainer";
-import CustomAlert from "../components/CustomAlert";
 
 const HomePage = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
   return (
     <>
       <Introduction />
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={5}>
+      <SimpleGrid columns={{ sm: 1, md: 2 }} spacing={5}>
         <HoverContainer>
           <Link to="/dev-projects">
             <Image src={dev} />
@@ -32,18 +22,7 @@ const HomePage = () => {
             <Image src={art} />
           </Link>
         </HoverContainer>
-
-        <HoverContainer>
-          <Image src={unknown} onClick={onOpen} />
-        </HoverContainer>
       </SimpleGrid>
-
-      <CustomAlert isOpen={isOpen} onClose={onClose}>
-        <VStack spacing={2}>
-          <Image src={locked} />
-          <Text as="b">this section is not yet available.</Text>
-        </VStack>
-      </CustomAlert>
     </>
   );
 };

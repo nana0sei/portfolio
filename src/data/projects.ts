87b978fe4d5bbc591b1d925737c021767e227e58
nana0sei <frimpongnanao@gmail.com>
2024-04-06 22:22:19 +0000

    rearranged projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,21 +5,21 @@ import portfolio from "../assets/portfolio.png";
 export default {
   data: [
     {
-      name: "gamehub",
-      description: "a lite version of rawg.io, a game discovery website.",
-      image: gamehub,
-      link: "https://game-hub-nana0sei.vercel.app/",
-      git: "https://github.com/nana0sei/game-hub",
-      tools: "react, typescript, rawg api, chakra ui",
-    },
-
-    {
       name: "spotify lite",
       description: "search, browse featured playlists and play previews.",
       image: spotify,
       link: "https://spotify-lite.vercel.app/",
       git: "https://github.com/nana0sei/spotify-lite",
       tools: "react, typescript, spotify api, chakra ui",
+    },
+
+    {
+      name: "gamehub",
+      description: "a lite version of rawg.io, a game discovery website.",
+      image: gamehub,
+      link: "https://game-hub-nana0sei.vercel.app/",
+      git: "https://github.com/nana0sei/game-hub",
+      tools: "react, typescript, rawg api, chakra ui",
     },
 
     {

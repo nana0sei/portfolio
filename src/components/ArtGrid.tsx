3246a72ecf5e3f2d749deffff62f9fd70f323e6b
nana0sei <frimpongnanao@gmail.com>
2024-04-21 11:39:55 +0000

    artgrid suspense

--- src/components/ArtGrid.tsx
+++ src/components/ArtGrid.tsx
@@ -1,13 +1,16 @@
-import { SimpleGrid } from "@chakra-ui/react";
+import { SimpleGrid, Skeleton } from "@chakra-ui/react";
 import images from "../data/images";
 import Artwork from "./Artwork";
+import { Suspense } from "react";
 
 const ArtGrid = () => {
   return (
     <>
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={5} py={2}>
         {images.data.map((i, index) => (
-          <Artwork img={i.image} description={i.description} key={index} />
+          <Suspense fallback={<Skeleton w="500px" h="500px" />} key={index}>
+            <Artwork img={i.image} description={i.description} />
+          </Suspense>
         ))}
       </SimpleGrid>
     </>

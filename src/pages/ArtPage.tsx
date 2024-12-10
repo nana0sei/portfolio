1743450772da72487df60caeab60bf595ceebbce
nana0sei <frimpongnanao@gmail.com>
2024-12-10 07:25:27 +0000

    removed box

--- src/pages/ArtPage.tsx
+++ src/pages/ArtPage.tsx
@@ -4,13 +4,13 @@ import ArtGrid from "../components/ArtGrid";
 
 const ArtPage = () => {
   return (
-    <Box h="calc(100vh)" alignContent="center">
+    <>
       <Center pb={2}>
         <Image src={arttitle} width="300px" />
       </Center>
 
       <ArtGrid />
-    </Box>
+    </>
   );
 };
 
